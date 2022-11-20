b184397687afe94eec7794b740e9d112b6ce1ba5
luoyuanfeng <luoyuanfeng@meituan.com>
2022-11-20 17:38:47 +0800

    initial commit

---
+++ src/main/java/com/bhu19/movie/reco/BhuMovieRecoApplication.java
@@ -0,0 +1,13 @@
+package com.bhu19.movie.reco;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BhuMovieRecoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BhuMovieRecoApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/bhu19/movie/reco/BhuMovieRecoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bhu19.movie.reco;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BhuMovieRecoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
