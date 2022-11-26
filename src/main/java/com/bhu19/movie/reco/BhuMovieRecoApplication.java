1594fb819d53d6aef611d5d2c09bc5e8b36436b1
luoyuanfeng <luoyuanfeng@meituan.com>
2022-11-26 17:47:42 +0800

    init project structure

--- src/main/java/com/bhu19/movie/reco/BhuMovieRecoApplication.java
+++ src/main/java/com/bhu19/movie/reco/BhuMovieRecoApplication.java
@@ -1,9 +1,11 @@
 package com.bhu19.movie.reco;
 
+import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
+@MapperScan(basePackages = {"com.bhu19.movie.reco.persistance.dao"})
 public class BhuMovieRecoApplication {
 
     public static void main(String[] args) {
